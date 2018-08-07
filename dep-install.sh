8a5db5fe363f36c8a3773ff3a94a163c38679f9d
Nithin <nithin@fragments.org>
2018-08-07 11:19:17 -0700

    added zos install

--- dep-install.sh
+++ dep-install.sh
@@ -23,6 +23,9 @@ echo "------Setting up environment"
 echo "------Installing truffle"
 npm install -g truffle@4.1.13
 
+echo "------Installing zos"
+npm install -g zos@1.1.0
+
 echo "------Installing ganache"
 npm install -g ganache-cli@6.1.0
 
