e4803ab481af10d3e4a4ec116aa35ebb14d76d4b
Nithin <nithin@fragments.org>
2018-08-13 13:36:04 -0700

    using truffle as a local dependency

--- lite-dep-install.sh
+++ lite-dep-install.sh
@@ -4,8 +4,5 @@ echo "------Setting up environment"
 echo "------Installing npx"
 npm install -g npx
 
-echo "------Installing truffle"
-npm install -g truffle@4.1.13
-
 echo "------Installing ganache"
 npm install -g ganache-cli@6.1.0
